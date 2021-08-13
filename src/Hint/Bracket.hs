fb0eac2a7795869f9a1d19ad0b3e2592f83f833b
sonfam <88331073+sonfam@users.noreply.github.com>
2021-08-13 12:38:51 +0700

    Update Bracket.hs

--- src/Hint/Bracket.hs
+++ src/Hint/Bracket.hs
@@ -190,10 +190,14 @@ bracket pretty isPartialAtom root = f Nothing
     -- 'x' actually need bracketing in this context?
     f (Just (i, o, gen)) v@(remParens' -> Just x)
       | not $ needBracket i o x, not $ isPartialAtom (Just o) x =
-          rawIdea Suggestion msg (getLoc v) (pretty o) (Just (pretty (gen x))) [] [r] : g x
+           rawIdea Suggestion msg (getLoc v) from to [] [r] : g x
       where
         typ = findType v
         r = Replace typ (toSS v) [("x", toSS x)] "x"
+        (from,to) = reduceJunks (pretty o) (Just (pretty (gen x)))
+        reduceJunks :: String -> Maybe String -> (String, Maybe String)
+        reduceJunks from (Just to) = (unlines f_, Just $ unlines t_)
+          where (f_,t_) = unzip $ filter (uncurry (/=)) $ zip (lines from) (lines to)
     -- Regardless of the context, there are no parentheses to remove
     -- from 'x'.
     f _ x = g x
